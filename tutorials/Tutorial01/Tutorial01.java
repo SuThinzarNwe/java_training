307929e797fa63be8057c71c81f04bf6bdd18760
SUTHINZARNWE-PC\SuThinzarNwe <scm.suthinzarnwe@gmail.com>
2022-02-23 15:11:54 +0630

    [main]tutorial01 added

---
+++ tutorials/Tutorial01/Tutorial01.java
@@ -0,0 +1,9 @@
+package tutorials.Tutorial01;
+public class Tutorial01 {
+    public static void main(String[] args) {
+
+        for (int x = 1; x <= 100; x++) {
+            System.out.println(x);
+        }
+    }
+}
---
+++ tutorials/Tutorial02/Tutorial02.java
@@ -0,0 +1,10 @@
+package tutorials.Tutorial02;
+
+public class Tutorial02 {
+    public static void main(String[] args) {
+       int x =1;
+
+       
+        
+    }
+}
